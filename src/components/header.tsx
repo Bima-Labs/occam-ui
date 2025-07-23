fbbb07c98c8491620829ed986a18144fd7d47dc4
devdatta <devdattanarawade97@gmail.com>
2025-07-23 21:46:42 +0530

    added options in navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { APP_NAME } from "@/lib/constants";
 import { ThemeToggleButton } from "@/components/theme-toggle-button";
 import Image from 'next/image'; // Import the Next.js Image component
 import bima_logo from '../../public/Bima_logo.svg';
+import Link from "next/link";
 
 export function Header() {
   return (
@@ -9,14 +10,21 @@ export function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex items-center">
           {/* Add the SVG here */}
-          <Image
-            src={bima_logo}
-            alt="Bima Logo" // It's good practice to add an alt attribute
-            width={96}// Specify a width (adjust as needed)
-            height={96} //Specify a height (adjust as needed)
-            className="mr-2 ml-7"// add some margin to the right
-          />
-        
+   <Link href="/">
+  <Image
+    src={bima_logo}
+    alt="Bima Logo"
+    width={96}
+    height={96}
+    className="mr-2 ml-7"
+  />
+</Link>
+          <Link
+            href="/options"
+            className="text-sm font-medium text-muted-foreground hover:text-primary transition-colors mx-5"
+          >
+            Options
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-2">
           <ThemeToggleButton />
